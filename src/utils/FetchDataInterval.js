1b0d2159d7f920c1550474303d6803194a14f1c1
watcharaphat <watcharaphat.3166@gmail.com>
2018-07-02 11:08:34 +0700

    correct auto refresh

--- src/utils/FetchDataInterval.js
+++ src/utils/FetchDataInterval.js
@@ -1,14 +1,17 @@
 import sleep from './Sleep';
 
 export default async function fetchDataInterval(m) {
-  try {
-    // eslint-disable-next-line
-    await m.fetchData();
-  } catch (err) {
-    // eslint-disable-next-line
-    console.error(`Cannot fetch ${m.url}`, err);
-  }
-
   // eslint-disable-next-line
-  await sleep(5000);
+  while (true) {
+    try {
+      // eslint-disable-next-line
+      await m.fetchData();
+    } catch (err) {
+      // eslint-disable-next-line
+      console.error(`Cannot fetch ${m.url}`, err);
+    }
+
+    // eslint-disable-next-line
+    await sleep(5000);
+  }
 }
