616130ba606fb5b84ffe5af642a1b3e72b8d1954
watcharaphat <watcharaphat.3166@gmail.com>
2018-07-03 12:42:00 +0700

    refactor latestBlock getter

--- src/store/index.js
+++ src/store/index.js
@@ -17,22 +17,21 @@ export default new Vuex.Store({
   },
   getters: {
     latestBlock: (state) => {
-      if (state.data.status.getData()) {
-        const data = state.data.status.getData();
-        const latestBlock = {
-          height: data.sync_info.latest_block_height,
-          hash: data.sync_info.latest_block_hash,
-          time: data.sync_info.latest_block_time,
-        };
-
-        return latestBlock;
-      }
-
-      return {
+      const latestBlock = {
         height: NaN,
         hash: NaN,
         time: NaN,
       };
+
+      if (state.data.status.getData()) {
+        const data = state.data.status.getData();
+
+        latestBlock.height = data.sync_info.latest_block_height;
+        latestBlock.hash = data.sync_info.latest_block_hash;
+        latestBlock.time = data.sync_info.latest_block_time;
+      }
+
+      return latestBlock;
     },
   },
   mutations: {
